ebc2a68250a9474e7f704a88a4666f53f37c7a51
Davi Tito <davitafernaberry.aluno@unipampa.edu.br>
2025-10-24 23:30:51 -0300

    Fix: Trocando a palavra processo por acesso

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ def main():
     processo = Processo(None)
     tamanho = int(input("Digite o tamanho máximo da moldura: "))
     fila = Fila(tamanho)
-    entrada = input("Digite os processos (ex: 1 2 33 14 2 6): ")
+    entrada = input("Digite os acessos (ex: 1 2 33 14 2 6): ")
     processo.simular(entrada, fila)
 
 if __name__ == "__main__":
